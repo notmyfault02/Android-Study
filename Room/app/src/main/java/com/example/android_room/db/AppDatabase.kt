07fa45e7ddc26e46981b04959451a89a7e8c9892
김영래 <rayray.yoyo@gmail.com>
2019-09-04 15:04:03 +0900

    Update Room Sources

--- Room/app/src/main/java/com/example/android_room/db/AppDatabase.kt
+++ Room/app/src/main/java/com/example/android_room/db/AppDatabase.kt
@@ -1,10 +1,28 @@
 package com.example.android_room.db
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.example.android_room.db.dao.MemoDao
 import com.example.android_room.db.entity.MemoEntity
 
 @Database(entities = arrayOf(MemoEntity::class), version = 1)
 abstract class AppDatabase: RoomDatabase() {
+    abstract fun MemoDao(): MemoDao
 
+    companion object {
+        private var INSTANCE: AppDatabase? = null
+
+        fun getInstance(context: Context): AppDatabase? {
+            if (INSTANCE == null) {
+                synchronized(AppDatabase::class) {
+                    INSTANCE = Room.databaseBuilder(context.applicationContext, AppDatabase::class.java, "memo.db").build()
+                }
+            }
+
+            return INSTANCE
+        }
+
+    }
 }
--- Room/app/src/main/java/com/example/android_room/db/dao/MemoDao.kt
+++ Room/app/src/main/java/com/example/android_room/db/dao/MemoDao.kt
@@ -2,11 +2,15 @@ package com.example.android_room.db.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.example.android_room.db.entity.MemoEntity
 
 @Dao
 interface MemoDao {
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertAll(vararg memo: MemoEntity)
+
+    @Query ("SELECT * FROM memoentity")
+    fun getAll(): Array<MemoEntity>
 }
